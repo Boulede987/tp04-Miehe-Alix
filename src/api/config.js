3422125b9e9cd2401c876ef3c51c6ae981dd38e8
MIEHE Alix <alixmiehe2004@gmail.com>
2025-11-13 13:55:15 +0100

    ajout de l'api, il est dnasun depot àpart pourle dev, j'ai oublié de l'ajouter...

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+module.exports =  {
+    ACCESS_TOKEN_SECRET : "EMMA123",
+    BDD : {
+    "host" :"dpg-d41mvchr0fns73cjb6i0-a.oregon-postgres.render.com",
+    "port" : "5432",
+    "user" : "api_postgres_ryy1_user",
+    "password" : "yL2wyfp1sGIkKBRwPQIkl1b44MmXvj9z",
+    "bdname" :"api_postgres_ryy1" 
+    }
+}
+
+// postgresql://api_postgres_ryy1_user:yL2wyfp1sGIkKBRwPQIkl1b44MmXvj9z@dpg-d41mvchr0fns73cjb6i0-a.oregon-postgres.render.com/api_postgres_ryy1
